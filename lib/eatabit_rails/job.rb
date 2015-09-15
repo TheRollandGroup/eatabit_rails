a79826ff23577299465053b7e83dcbb35e976cf0
Alex Sleighter <alex.sleighter@gmail.com>
2015-09-15 12:48:29 -0400

    symbol hash

--- lib/eatabit_rails/job.rb
+++ lib/eatabit_rails/job.rb
@@ -40,7 +40,7 @@ module EatabitRails
     
     def to_hash
       hash = {}
-      instance_variables.each {|var| hash[var.to_s.delete("@")] = instance_variable_get(var) }
+      instance_variables.each {|var| hash[var.to_s.delete("@").to_sym] = instance_variable_get(var) }
       hash
     end
 
